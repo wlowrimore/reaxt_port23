163fff2faa9b4881ed05da58fc26dd8c4cf68363
William Lowrimore <wlowrimore@gmail.com>
2023-09-19 00:08:47 -0500

    reworks the responsive styling

--- app/components/home/page.jsx
+++ app/components/home/page.jsx
@@ -14,11 +14,10 @@ import "./home.css";
 
 const Home = () => {
   return (
-    <main className="min-h-screen bg-custombg1 bg-no-repeat bg-fixed bg-cover bg-center pt-8 md:py-[6rem] lg:py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:ml-[5rem] 2xl:mx-[20rem] 2xl:mt-[12rem]">
-        {/* left section of grid */}
-
-        <section className="flex flex-col md:col-span-4 lg:col-span-1 justify-center lg:items-start items-center md:pt-[4rem] md:pb-[6rem] mx-6 md:mx-[4rem]">
+    <main className="min-h-screen bg-custombg1 bg-no-repeat bg-fixed bg-cover bg-center flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:gap-40 mx-auto">
+        {/* left/top section of grid */}
+        <section className="flex flex-col justify-center items-center lg:items-start mx-auto">
           <div>
             <p className="text-2xl text-center md:text-start md:text-3xl xl:text-5xl text-orange-400">
               Hello, I&apos;m
@@ -29,18 +28,16 @@ const Home = () => {
           </div>
 
           <div>
-            <h1 className="uppercase flex flex-col md:flex-row w-full text-xl md:text-start text-center md:text-2xl xl:text-4xl text-orange-400">
+            <h1 className="uppercase flex flex-col md:flex-row w-full text-xl md:text-start text-center md:text-2xl xl:text-3xl text-orange-400">
               Web Developer
               <span className="text-gray-200">Front end</span>
             </h1>
           </div>
 
           {/* typing component */}
-
           <DescAnimate />
 
           {/* buttons group */}
-
           <div className="flex h-full items-center 2xl:mt-8">
             <button
               href="/contact"
@@ -64,57 +61,56 @@ const Home = () => {
           </div>
         </section>
 
-        {/* right section of grid */}
-
-        <section className="flex flex-col justify-center items-center mx-6 md:pl-[4rem] md:py-[2rem]">
+        {/* right/bottom section of grid */}
+        <section className="flex justify-center items-center px-4">
           <Image
             src="/images/profile_img.jpg"
             width={500}
             height={500}
             alt="image representing the author"
-            className="self rounded-md md:ml-[20rem] lg:ml-0 md:rounded-full border md:border-4 border-orange-200"
+            className="self rounded-md md:rounded-full border md:border-4 border-orange-200"
           />
         </section>
       </div>
 
       {/* list of traits */}
-      <div className="flex justify-center w-full lg:pl-[1rem] lg:pr-[2.2rem] 2xl:pl-[14.5rem] 2xl:pr-[17rem]">
-        <ul className="w-full flex flex-col lg:flex-row items-center justify-center mx-4 my-6 text-[1.3rem] tracking-wide text-gray-100 uppercase space-y-2">
+      <div className="flex justify-center items-center">
+        <ul className="flex flex-col lg:flex-row lg:space-x-12 lg:pt-12 justify-center items-center pt-6 text-[1.2rem] tracking-wider text-gray-100 leading-relaxed uppercase">
           <Fade cascade direction="left" damping={1} triggerOnce="true">
-            <li className="flex w-full justify-center mt-2">
+            <li className="flex">
               <Image
                 priority
                 src={CodeIcon}
                 width={32}
                 height={32}
                 alt="code symbol svg"
-                className="pr-[0.5rem]"
+                className="pr-2"
               />
               meticulous code
             </li>
           </Fade>
           <Fade cascade direction="right" damping={1} triggerOnce="true">
-            <li className="flex w-full justify-center">
+            <li className="flex">
               <Image
                 priority
                 src={DesignIcon}
                 width={32}
                 height={32}
                 alt="design svg"
-                className="pr-[0.5rem]"
+                className="pr-2"
               />
               creative design
             </li>
           </Fade>
           <Fade cascade direction="left" damping={0.3} triggerOnce="true">
-            <li className="flex w-full justify-center">
+            <li className="flex">
               <Image
                 priority
                 src={TeamIcon}
                 width={32}
                 height={32}
                 alt="team collaboration svg"
-                className="pr-[0.5rem]"
+                className="pr-2"
               />
               collaboration
             </li>
